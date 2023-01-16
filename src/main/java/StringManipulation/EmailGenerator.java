39a338921f59ed666c2d441800c4e5832763f238
Aaminah1 <Aaminah1@localhost>
2023-01-16 19:18:30 +0200

    so i've finished the practical, but i made it toooo complex :o

---
+++ src/main/java/StringManipulation/EmailGenerator.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StringManipulation;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aaminah1
+ */
+public class EmailGenerator {
+    public static void main(String[] args) {
+        String fullname = JOptionPane.showInputDialog("Enter your full name");
+        String birthdate = JOptionPane.showInputDialog("Enter your year of birth");
+        
+        String email = "";
+        
+        String[] nameArray = fullname.split(" ");
+        
+        String first_name = nameArray[0].substring(0, 3);
+        email += first_name;
+        String last_name = nameArray[nameArray.length - 1].substring(0, 3);
+        email += last_name;
+        email += birthdate.substring(2, 4);
+        email += "@gmail.com";
+        
+        System.out.println(email);
+                        
+    }
+    
+}
