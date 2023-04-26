c3cb3b2d4427dfe0229ddfca6f5ca0c29c56d056
Aaminah1 <Aaminah1@localhost>
2023-04-26 06:43:45 +0200

    june prac

--- src/main/java/Arrays/ArrayMethods.java
+++ src/main/java/Arrays/ArrayMethods.java
@@ -30,21 +30,99 @@ public class ArrayMethods {
         arr[10] = 25;
         size = 11;
 
-       
+        add(18);
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i]);
+        }
+        
+        System.out.println("\n");
+        remove(8);
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i]);
+        }
+        
+        System.out.println("\n");
+        removeDuplicate();
         for (int i = 0; i < size; i++) {
             System.out.println(arr[i]);
         }
 
     }
     
-    public void add(int num)
+    public static void add(int num)
     {
         int pos = 0;
         
         
         while (num > arr[pos])
         {
-            33
+            pos++;
+        }
+        shiftRight(pos);
+        arr[pos] = num;
+        size++;
+        
+    }    
+    
+    private static void shiftRight(int pos)
+    {
+        for (int i = size -1; i >= pos; i--) 
+        {
+            arr[i + 1] = arr[i];
+        }
     }
     
+    public static void remove(int num)
+    {
+        shiftLeft(search(num));
+        size--;
+    }
+    
+    private static void shiftLeft(int index)
+    {
+        for (int i = index; i < size; i++) 
+        {
+            arr[i] = arr [i + 1];
+        }
+    }
+    
+    private static int search(int num)
+    {
+        int start = 0;
+        int end = size - 1;
+        
+        while (start <= end)
+        {
+            int mid = (start + end)/2;
+            
+            
+            if (arr[mid] == num)
+            {
+                return mid;
+            }
+            else if (arr[mid] > num)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void removeDuplicate()
+    {
+        for (int i = 0; i < size - 1; i++) 
+        {
+            while (arr[i] == arr[i + 1])
+            {
+                shiftLeft(i + 1);
+                size--;
+            }
+        }
+    }
 }
+
